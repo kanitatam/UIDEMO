647d729cb0298d53e69de71beffad6d3375f3d41
kanitatam <peedee3016@gmail.com>
2018-05-22 16:53:09 +0700

    loginModel.1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,13 +3,14 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ButtonBar;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import javax.swing.*;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -21,33 +22,55 @@ public class Controller implements Initializable {
     @FXML
     private PasswordField password;
     @FXML
-    private ButtonBar btnLogin;
+    private Button btnLogin;
     @FXML
     private Label dbStatus;
 
     private loginModel loginModel = new loginModel();
 
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        if (this.loginModel.isDatabaseConnection()){
+        if (this.loginModel.isDatabaseConnection()) {
             this.dbStatus.setText("Connected to DB.");
-        }else{
+        } else {
             this.dbStatus.setText("Not Connect to DB.");
         }
+
+
+
     }
+
     @FXML
     public void Login(ActionEvent event){
+//        try {
+//            if (id.equals(username.getText()) && pass.equals(password.getText())) {
+//                JOptionPane.showMessageDialog(null,
+//                        "Welcome to MIT SAIYAI APP");
+//            } else {
+//                JOptionPane.showMessageDialog(null,
+//                        "Your username or password is invalid.",
+//                        "Warning Message",
+//                        JOptionPane.WARNING_MESSAGE);
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         try {
-            if (id.equals(username.getText()) && pass.equals(password.getText())) {
-                JOptionPane.showMessageDialog(null, "Welcome to MIT SATYAI APP");
+            if (this.loginModel.isLogin(this.username.getText(), this.password.getText())) {
+                JOptionPane.showMessageDialog(null,
+                        "Welcome to MIT SAIYAI APP");
             } else {
-                JOptionPane.showMessageDialog(null, "Your username or password is invalid","Warning Message",JOptionPane.WARNING_MESSAGE);
+                JOptionPane.showMessageDialog(null,
+                        "Your username or password is invalid.",
+                        "Warning Message",
+                        JOptionPane.WARNING_MESSAGE);
             }
-
-        } catch (Exception e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-    }//Login
-}//class
 
+
+    }//Login
+
+}//class
--- src/sample/loginModel.java
+++ src/sample/loginModel.java
@@ -3,26 +3,57 @@ package sample;
 import dbUtil.dbConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class loginModel {
 
     Connection connection;
-
-    public  loginModel(){
     //constructor
-
+    public loginModel(){
         try {
             this.connection = dbConnection.getConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        if (this.connection == null)System.exit(1);
+        if (this.connection ==null) System.exit(1);
     }
-
 
     public boolean isDatabaseConnection(){
         return this.connection != null;
+    }
+
+    public boolean isLogin(String username, String password) throws SQLException {
+        PreparedStatement pr = null;
+        ResultSet rs = null;
+
+        String sql = "select * from admin where username = ? and password = ?";
+        try {
+            pr = this.connection.prepareStatement(sql);
+            pr.setString(1,username);
+            pr.setString(2,password);
+
+            rs = pr.executeQuery();
+            if (rs.next()) {
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
         }
+        finally {
+            pr.close();
+            rs.close();
+        }
+
+    }
+
+
+
+
+
+
 
 }//class
